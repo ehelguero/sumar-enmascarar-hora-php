bd1604607c7ae217c999b0337ebecb5826c17fa2
Jonatan <jrperdomo.24@gmail.com>
2012-09-04 02:35:54 +0200

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,21 +1,20 @@
 <?php
-#1- pedir la hora via formular
+#1- pedir la hora via formulario
 if($_GET['hora'] && $_GET['hora']!=" "){
 	$hora = $_GET['hora'];
 
 	#2- guardar la fecha actual fomato Y-m-d
 	$fecha = date('Y-m-d');
 				
-	#3- concatenar la variable fecha y la hora que se pidio en l formulario. en este ejemplo es '07:00'
+	#3- concatenar la variable fecha y la hora que se pidio en el formulario.
 	$fecha = date_create($fecha . $hora);
 
 	#4- le damos formato a la fecha
-	$fecha = date_format($fecha, 'd/m/Y H:i'); // 's' segundos
+	$fecha = date_format($fecha, 'd/m/Y H:i'); 
 				
 	#5- guardamos el dia para saber cuando es viernes==5
-	$control_dia = date('w'); // , strtotime('+3 days');
-	//echo "A partir de esta fecha: " . $fecha . ", calculamos la hora de salida <br/><br/>";
-
+	$control_dia = date('w'); 
+	
 	if($control_dia == 5){
 	echo "+7 horas y 00 minutos --> ". date('H:i',strtotime($fecha . '+7 hour'));	
 	}
